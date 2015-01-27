4c1ce84c32a590b114ab5fb76ea50efb33244528
kenwilcox <wilcoxk@gmail.com>
2015-01-27 00:08:01 -0700

    using an array of Dictionary to hold dummy data

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
   
+  var taskArray:[[String:String]] = []
+  //var taskArray:[Dictionary<String,String>] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let task1:Dictionary<String, String> = ["task": "Study Swift", "subtask": "iOS Docs, github, etc.", "date": "01/30/2015"]
+    let task2:Dictionary<String, String> = ["task": "Eat Dinner", "subtask": "Burgers", "date": "01/30/2015"]
+    let task3:Dictionary<String, String> = ["task": "Gym", "subtask": "Leg day", "date": "01/31/2015"]
+    
+    taskArray = [task1, task2, task3]
+    // Get rid of the bogus empty rows
+    tableView.tableFooterView = UIView()
+    tableView.reloadData()
   }
   
   override func didReceiveMemoryWarning() {
@@ -29,14 +41,16 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
+    return taskArray.count
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let taskDict:Dictionary = taskArray[indexPath.row]
     var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskTableViewCell
-    cell.taskLabel.text = "Study Swift"
-    cell.descriptionLabel.text = "iOS Docs, github, etc."
-    cell.dateLabel.text = "01/30/2015"
+    cell.taskLabel.text = taskDict["task"]
+    cell.descriptionLabel.text = taskDict["subtask"]
+    cell.dateLabel.text = taskDict["date"]
     return cell
   }
 }
