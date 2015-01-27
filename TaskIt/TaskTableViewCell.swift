f1b01b9ec608129af2e5a51bf1fa18f4310a18b1
kenwilcox <wilcoxk@gmail.com>
2015-01-26 23:50:41 -0700

    added label outlets

--- TaskIt/TaskTableViewCell.swift
+++ TaskIt/TaskTableViewCell.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class TaskTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  
+  @IBOutlet weak var taskLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
 }
