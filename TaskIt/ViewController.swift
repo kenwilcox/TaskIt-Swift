5ce63be151b423cc0d6b178f7c81c87dfbe0b0b2
kenwilcox <wilcoxk@gmail.com>
2015-01-26 23:11:39 -0700

    added tableView Outlet, conform to UITableViewDelegate and UITableViewDataSource as extensions

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +25,21 @@ class ViewController: UIViewController {
 
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    
+  }
+}
