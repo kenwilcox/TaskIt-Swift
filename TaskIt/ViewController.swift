f8ed82d419a7f3e5c99a05f58e106d05ad7cc504
kenwilcox <wilcoxk@gmail.com>
2015-01-26 23:42:54 -0700

    adding some dummy cells

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -29,11 +29,12 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return 5
   }
   
   func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskTableViewCell
+    return cell
   }
 }
 
