d6e75ba3b6b119579b706845149714ad33ae2d6d
kenwilcox <wilcoxk@gmail.com>
2015-01-26 23:31:25 -0700

    Added TaskTableViewCell and made it the prototype cell class, added myCell as the identifier

---
+++ TaskIt/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  TaskIt
+//
+//  Created by Kenneth Wilcox on 1/26/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
