3e225ba95fc4663b64e537bb91da9d05101c6e0e
kenwilcox <wilcoxk@gmail.com>
2015-01-29 22:26:03 -0700

    explicitly making sure the data is reloaded

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -33,6 +33,11 @@ class ViewController: UIViewController {
     //tableView.reloadData()
   }
   
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    tableView.reloadData()
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
