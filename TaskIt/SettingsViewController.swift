67dc5ffd7cd00ccafa87efcab4c5db9875790ab3
Ken Wilcox <wilcoxk@gmail.com>
2015-02-16 22:04:32 -0700

    added SettingsViewController viewDidLoad

--- TaskIt/SettingsViewController.swift
+++ TaskIt/SettingsViewController.swift
@@ -18,11 +18,58 @@ class SettingsViewController: UIViewController {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
+    self.capitalizeTableView.delegate = self
+    self.capitalizeTableView.dataSource = self
+    self.capitalizeTableView.scrollEnabled = false
+    
+    self.completeNewTodoTableView.delegate = self
+    self.completeNewTodoTableView.dataSource = self
+    self.completeNewTodoTableView.scrollEnabled = false
+    
+    self.title = "Settings"
+    
+    let version = NSBundle.mainBundle().infoDictionary?["CFBundleShortVersionString"] as? String
+    let build = NSBundle.mainBundle().infoDictionary?["CFBundleVersion"] as? String
+    switch (version, build) {
+    case let (.Some(version), .Some(build)):
+      self.versionLabel.text = "\(version).\(build)"
+    default:
+      self.versionLabel.text = "??"
+    }
+    
+    var doneButton = UIBarButtonItem(title: "Done", style: .Plain, target: self, action: Selector("doneBarButtonItemPressed:"))
+    self.navigationItem.leftBarButtonItem = doneButton
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  func doneBarButtonItemPressed(barButtonItem: UIBarButtonItem) {
+    self.navigationController?.popViewControllerAnimated(true)
+  }
+}
+
+// MARK: UITableViewDataSource
+extension SettingsViewController: UITableViewDataSource {
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 0
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    var cell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskTableViewCell
+    return cell
+  }
+}
+
+// MARK: UITableViewDelegate
+extension SettingsViewController: UITableViewDelegate {
   
 }
